b2612651c5c0afeec434763e154e9827867b36c4
hirkali <eren.demir1826@gmail.com>
2023-09-20 21:56:12 +0300

    pong game

---
+++ PongGame/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float startSpeed;
+    public float extraSpeed;
+    public float maxExtraSpeed;
+
+
+    private int hitCounter = 0;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+ 
+}
